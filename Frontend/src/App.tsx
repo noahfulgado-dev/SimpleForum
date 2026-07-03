d0a44cd348cbe9d66ea0a3d8ee5d22bf8e05b44e
Zeq-Dev <zeqdev.1@gmail.com>
2026-07-03 13:04:33 +0800

    React reinstalled (js to tsx)

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/globals.css'
+
+function App() {
+  return (
+    <>
+      <div className="w-dvw h-dvh bg-amber-200">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
