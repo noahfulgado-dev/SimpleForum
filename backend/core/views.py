60521994ac24658f60487f25b3df367a7671580c
Ken Perez <rken.perez@gmail.com>
2026-07-21 22:42:04 +0800

    remove decorator from /health/ to accept all methods for UptimeRobot

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,7 +1,5 @@
 from django.http import JsonResponse
-from django.views.decorators.http import require_http_methods
 
 
-@require_http_methods(["GET", "HEAD"])
 def health(request):
     return JsonResponse({"status": "ok"})
