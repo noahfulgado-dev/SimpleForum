95a8fd55c00defc6d26217bc92cce65c79ab16ca
Ken Perez <rken.perez@gmail.com>
2026-07-26 17:54:21 +0800

    fix: navbar profile removed border

--- frontend/src/components/ui/navbar.tsx
+++ frontend/src/components/ui/navbar.tsx
@@ -42,7 +42,7 @@ export function Navbar() {
                         <img src={bellIcon} alt="Bell" className="w-7 h-7" />
                     </div>
                     <div className="relative group w-8 h-8 flex items-center justify-center transition-all duration-300 ease-in-out cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
-                        <img src={user?.avatar || defaultAvatar} alt="Avatar" className="w-8 h-8 border border-gray-800 rounded-full" />
+                        <img src={user?.avatar || defaultAvatar} alt="Avatar" className="w-8 h-8 rounded-full" />
                         <div className="absolute rounded-full inset-0 bg-gray-900/0 transition-colors duration-300 group-hover:bg-[#e5e5e5]/30"></div>
                     </div>
                     {isOpen && (
