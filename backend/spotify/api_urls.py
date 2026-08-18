a9f754eda48673ef3e714930a92f6cfc6effc032
Ken Perez <115914124+rkenbperez@users.noreply.github.com>
2026-08-18 10:21:10 +0800

    fix: cookie-free spotify oauth connect via signed state (#166)

--- backend/spotify/api_urls.py
+++ backend/spotify/api_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from spotify.views import NowPlayingView, SpotifyAuthorizeView
+from spotify.views import NowPlayingView, SpotifyAuthorizeView, SpotifyCallbackView
 
 urlpatterns = [
     path('now-playing/', NowPlayingView.as_view(), name='now-playing'),
     path('authorize/', SpotifyAuthorizeView.as_view(), name='spotify-authorize'),
+    path('callback/', SpotifyCallbackView.as_view(), name='spotify-callback'),
 ]
